a0edeb5116598532b959ef38dbad6d612133353a
Laurin Prajescu <laurinprajescu@gmail.com>
2016-10-26 17:38:23 +0100

    Quote calculator added

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,29 @@
 angular.module('RouteControllers', [])
 
     .controller('HomeController', function($scope) {
-        
+
+    	
     })
+
+    .controller('CalcController', function($scope) {
+
+    	$scope.result = function() {
+
+    		if ($scope.value < 300) {
+
+    		return $scope.value * 1.45
+
+    		}
+
+    		else {
+
+    			return $scope.value * 1.45 / 1.10;
+
+    		}
+    	}
+    })
+
+
 
     .controller('ServicesSebController', function($scope) {
         
