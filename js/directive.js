4c121e8135b2cedadd2a0d3c432b32745d967585
Laurin Prajescu <laurinprajescu@gmail.com>
2016-11-21 11:28:54 +0000

    new directive

--- js/directive.js
+++ js/directive.js
@@ -4,5 +4,14 @@ angular.module('SebAppDirectives',[])
             restrict: 'EA',
             templateUrl: 'templates/directives/jobs.html'
         };
+    })
+
+    .directive('contactusmap', function () {
+        return {
+            restrict: 'EA',
+            templateUrl: 'templates/directives/contactus-map.html'
+        };
     });
+
+
     
